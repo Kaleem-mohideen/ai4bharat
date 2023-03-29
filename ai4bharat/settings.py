c7a829e1bbe2caac0e1d77d2af9f2a4fdb67e23c
Mir Kaleem Mohideen <32511479+Kaleem-mohideen@users.noreply.github.com>
2023-03-29 16:14:23 +0530

    added static and media path

--- ai4bharat/settings.py
+++ ai4bharat/settings.py
@@ -134,8 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
