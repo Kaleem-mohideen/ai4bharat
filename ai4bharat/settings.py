cbeeed3c8a0c7d1bf12e88ba9423af7a68a74cb3
Mir Kaleem Mohideen <32511479+Kaleem-mohideen@users.noreply.github.com>
2023-03-29 17:19:44 +0530

    added static

--- ai4bharat/settings.py
+++ ai4bharat/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
